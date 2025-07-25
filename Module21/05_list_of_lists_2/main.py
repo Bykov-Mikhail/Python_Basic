b276f727b64e0481a75e48c54f063dcce200e907
Mikhail Bykov <mipa123123@mail.ru>
2025-07-25 18:22:53 +0500

    add_homework_m21

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,12 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+def open_list(list_of_lists):
+    if not isinstance(list_of_lists, list):
+        return [list_of_lists]
+    result = []
+    for elem in list_of_lists:
+        result.extend(open_list(elem))
+    return result
+
+print(open_list(nice_list))
--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+def recurs_sup_list(sup_list):
+    if not sup_list:
+        return []
+    result = []
+    result.extend(support_sort(sup_list))
+    return result
+
+def support_sort(list_num):
+    sup_elem = list_num[-1]
+    less_sup = []
+    equally_sup = []
+    more_sup = []
+    for elem in list_num:
+        if elem < sup_elem:
+            less_sup.append(elem)
+        elif elem == sup_elem:
+            equally_sup.append(elem)
+        elif elem > sup_elem:
+            more_sup.append(elem)
+    return quick_sort(less_sup, equally_sup, more_sup)
+
+def quick_sort(less, equal, more):
+    recurs_sup_list(less)
+    recurs_sup_list(more)
+
+    return recurs_sup_list(less) + equal + recurs_sup_list(more)
+
+numbers = [5, 8, 9, 4, 2, 9, 1, 8]
+
+print(support_sort(numbers))
