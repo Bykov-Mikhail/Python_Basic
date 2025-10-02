8782de1af2dc230e6a4c72e9afeaa75c150bd5ae
Михаил Быков <mipa123123@mail.ru>
2025-10-02 14:27:03 +0300

    add_homework_26

--- Module26/04_linked_list/main.py
+++ Module26/04_linked_list/main.py
@@ -1,1 +1,71 @@
-# TODO здесь писать код
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current.data
+            current = current.next
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+             self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+
+    def __str__(self):
+        if self.head is None:
+            return '[]'
+        main = []
+        current = self.head
+        while current is not None:
+            main.append(str(current.data))
+            current = current.next
+        return '[' + ', '.join(main) + ']'
+
+    def get(self, index):
+        current = self.head
+        current_index = 0
+        while current is not None:
+            if current_index == index:
+                return current.data
+            current = current.next
+            current_index += 1
+
+    def remove(self, index):
+        current = self.head
+        current_index = 0
+        while current is not None:
+            if index == 0:
+                del_value = current.data
+                self.head = current.next
+                return del_value
+
+            if current_index == index - 1:
+                del_value = current.next.data
+                current.next = current.next.next
+                return del_value
+
+            current = current.next
+            current_index += 1
+
+my_list = LinkedList()
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+print('Текущий список:', my_list)
+print('Получение третьего элемента:', my_list.get(2))
+print('Удаление второго элемента.')
+my_list.remove(1)
+print('Новый список:', my_list)
+
--- Module26/05_logs_processing/main.py
+++ Module26/05_logs_processing/main.py
@@ -1,10 +1,16 @@
-# TODO здесь писать код
-
-
+import os
+def error_log_generator(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding = 'utf-8') as file:
+            for line in file:
+                if 'ERROR' in line.strip():
+                    yield line
+    else:
+        return 'Такого пути не существует'
 # При помощи модуля os (и функции join) сформируйте пути до файлов work_logs.txt и output.txt в папке data
 # (output.txt может не быть в папке data, но его нужно будет там создать, при помощи кода)
-input_file_path = ...
-output_file_path = ...
+input_file_path = os.path.join('data', 'work_logs.txt')
+output_file_path = os.path.join('data', 'output.txt')
 # Документация по join https://docs-python.ru/standart-library/modul-os-path-python/funktsija-join-modulja-os-path/
 
 # Не забудьте проверить наличие файлов перед тем как начать работу с ними
