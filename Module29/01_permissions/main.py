e6707db1f903f6b0f2afb5dc03f5048d00b6e2a5
Mikhail Bykov <mipa123123@mail.ru>
2025-11-11 11:40:27 +0500

    add_module_29

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+from functools import wraps
+from typing import Any, Callable, List
+
+def check_permission(required_permission: List[str]) -> Callable:
+    """
+    Декоратор, который проверяет права для выполнения функции
+    :param required_permission: требуемые права
+    :return: Декорированная функция
+    """
+    def decor(func: Callable) -> Callable:
+        @wraps(func)
+        def wrap(*args : Any, **kwargs : Any) -> Any:
+            if not any(perm in user_permissions for perm in required_permission):
+                raise PermissionError(f"у пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}")
+            return func(*args, **kwargs)
+        return wrap
+    return decor
+
+user_permissions = ['admin']
+
+@check_permission(['admin', 'moderator'])
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission(['user_1', 'moderator'])
+def add_comment():
+    print('Добавляем комментарий')
+
+delete_site()
+add_comment()
--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+from typing import Callable
+class App:
+    """Класс представляет веб приложение с маршрутизацией"""
+    def __init__(self):
+        """Инициализирует пустой словарь маршрутов"""
+        self.routes = {}
+
+    def get(self, path: str) -> Callable:
+        """
+        Возвращает функцию указанного пути
+        :param path: Путь (URL - маршрут)
+        :return: Функция обработчик или None, если путь не найден.
+        """
+        return self.routes.get(path)
+
+app = App()
+
+def callback(path: str) -> Callable:
+    """
+    Декоратор, который привязывает путь к функции
+    :param path: строка(путь)
+    """
+    def decor(func: Callable) -> Callable:
+        """
+        Декорируем функцию
+        :param func: декорируемая функция
+        :return: декорированная функция
+        """
+        app.routes[path] = func
+        return func
+    return decor
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
